da42096bfdf9cf50a9619fc400c843ae6876549c
Heiko Mueller <heiko.muller@gmail.com>
2020-01-12 13:51:14 -0500

    Remove unused code

--- benchmark/code/plot.py
+++ benchmark/code/plot.py
@@ -17,18 +17,6 @@ from sklearn.metrics import roc_auc_score
 from sklearn.metrics import roc_curve
 
 
-def get_accuracy(outputs, labels):
-    """Compute the accuracy for all tagger outputs."""
-    # Flatten array
-    labels = labels.ravel()
-    # Round elements of the array to the nearest integer.
-    outputs = np.rint(outputs)
-    outputs = np.asarray(outputs)
-    labels = np.asarray(labels)
-    outputs = outputs.flatten()
-    return np.sum(outputs == labels) / float(len(labels))
-
-
 def get_median_model(true_labels, tagger_outputs):
     """Get median model sorted by auc."""
     taggers_median = dict()
@@ -46,7 +34,7 @@ def get_median_model(true_labels, tagger_outputs):
     return taggers_median
 
 
-def plot(tagger_results, input_truth_file, tag_eff, output_file):
+def plot(tagger_results, input_truth_file, output_file, show_plot=False):
     """Create plot containing ROC corves for all tagger outputs. Saves the plot
     to the given output file.
 
@@ -58,9 +46,10 @@ def plot(tagger_results, input_truth_file, tag_eff, output_file):
         name ('name') and the path to the tagger result file ('file')
     input_truth_file: string
          Path to the file (labels.pkl) that contains the ground truth data
-    tag_eff: float
     output_file: string
         Path to the output file where the plot is stored
+    show_plot: boolean, optional
+        Show the created plot after saving it
     """
     # Load groundtruth labels
     with open(input_truth_file, 'rb') as f:
@@ -74,30 +63,15 @@ def plot(tagger_results, input_truth_file, tag_eff, output_file):
         t_name = tagger['name']
         tagger_names.append(t_name)
         with open(tagger['file'], 'rb') as f:
-            tagger_outputs[t_name] = np.asarray(pickle.load(f))
+            tagger_outputs[t_name] = np.load(f)
     tagger_median = get_median_model(true_labels, tagger_outputs)
     # Shortcut to tagger names and outputs
-    # ???
-    aucs = []
-    errors = []
-    accuracies = []
     base_tpr = np.linspace(0.05, 1, 476)
-    # Get tag efficiency working point
-    if tag_eff == 0.5:
-        point = 225
-    elif tag_eff == 0.3:
-        point = 125
-    elif tag_eff == 0.4:
-        point = 175
     # Define line styles for plot
     linestyles = ['-', '--', '-.', ':'] * 10
     for t_name in tagger_names:
         # Tagger output array
         t_out = tagger_median[t_name]
-        # Tagger accuracies, AUC, and ROC curve
-        accuracies.append(get_accuracy(t_out, true_labels))
-        aucs.append(roc_auc_score(true_labels, t_out))
-
         fpr, tpr, thresholds = roc_curve(
             true_labels,
             t_out,
@@ -106,10 +80,6 @@ def plot(tagger_results, input_truth_file, tag_eff, output_file):
         )
         # Get error
         inv_fpr = interp(base_tpr, tpr, fpr)
-        len_bg = len(true_labels[true_labels == 0])
-        bg_eff = inv_fpr[point]
-        error_rel = np.sqrt(1./(bg_eff*len_bg))
-        errors.append(error_rel*1./inv_fpr[point])
         # Create plot and save to file (if filename is given)
         plt.rcParams['figure.figsize'] = 9, 9
         plt.plot(
@@ -128,7 +98,8 @@ def plot(tagger_results, input_truth_file, tag_eff, output_file):
         plt.legend(loc="best")
         plt.grid(which='both', axis='both', linestyle='--')
         plt.savefig(output_file)
-        plt.show()
+        if show_plot:
+            plt.show()
 
 
 if __name__ == '__main__':
@@ -145,6 +116,6 @@ if __name__ == '__main__':
     plot(
         tagger_results=results,
         input_truth_file=args[1],
-        tag_eff=0.3,
-        output_file=args[2]
+        output_file=args[2],
+        show_plot=True
     )
